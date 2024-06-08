81550858b16f08813931505a3dd1b2f516bd2e4a
sureshauti1 <127009394+sureshauti1@users.noreply.github.com>
2024-06-08 17:11:39 +0000

    01_variables.js_addition

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=134567
+let accountEmail="sureshauti98@gmail.com"
+var accountPassword="1234"
+accountCity="Pune"
+
+let accountState;
+// accountId=345
+
+console.log(accountId)
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
