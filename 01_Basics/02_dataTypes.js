d2401d4894206811591aaa5650b6f640eab6d401
sureshauti1 <127009394+sureshauti1@users.noreply.github.com>
2024-06-09 08:23:15 +0000

    conversion of datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+// "use strict"; //treat all JS code as newer version
+
+// // alert(3+3) we are using nodejs, not browser
+
+// let name="suresh"
+
+// let age=18
+
+// let isLoggedIn=false
+
+//number => 2 to power 53
+//bigint
+//string => " "
+//boolean => true
+//null => standalone value
+//undefinde => 
+//symbol =>unique
+// console.log(typeof("suresh"))
+
+console.log(typeof undefined)
+
+
+
---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score=true
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+let valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber)
+
+// console.log(valueInNumber)
+
+let isLoggedIn=true
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+
+
+
+
